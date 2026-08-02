33a35650e866df0eed8aec6a29cfcf9a875369a0
varshan-tech1420 <varshan387s@gmail.com>
2026-08-02 15:59:09 +0530

    Added bootstrap UI,login and register page

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login_view(request):
+    return render(request, 'login.html')
